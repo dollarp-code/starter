e1fba832d0804020a7ca8910077ec513f4f872ab
dolapo <husseindollarp@yahoo.com>
2024-02-19 20:23:59 -0800

    Learnt the various types of datatypes in javascript

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-let js = "Amazing";
-console.log(40 + 8 + 23 - 10);
+/*
 
-console.log("Hussein");
+//declaring a variable in javascript
+// syntax: let variableName = value*/
 
-// declaring a variable in javascript
-// syntax: let variableName = value
-
-let firstName = "Hussein";
+/*let firstName = "Hussein";
 let secondName = "qudus";
 let thirdName = "Dolapo";
 
@@ -21,4 +18,21 @@ let population = "226";
 
 console.log(country);
 console.log(continent);
-console.log(population);
+console.log(population);*/
+
+//Data types in javascript
+
+let javascriptIsFun = true;
+console.log(typeof javascriptIsFun);
+
+javascriptIsFun = false;
+console.log(typeof javascriptIsFun);
+
+let age;
+console.log(typeof age);
+
+age = 36;
+console.log(typeof age);
+
+let country = "Nigeria";
+console.log(typeof country);
