0babf023dc3acf2d51f6bbab75319af6faee282d
dolapo <husseindollarp@yahoo.com>
2024-02-19 19:41:11 -0800

    Learnt how to declare variables in javascript

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 let js = "Amazing";
-if (js === "Amazing") alert("javascript is amazing");
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Hussein");
+
+// declaring a variable in javascript
+// syntax: let variableName = value
+
+let firstName = "Hussein";
+let secondName = "qudus";
+let thirdName = "Dolapo";
+
+console.log(firstName);
+console.log(secondName);
+console.log(thirdName);
+
+// Assignment
+let country = "Nigeria";
+let continent = "Africa";
+let population = "226";
+
+console.log(country);
+console.log(continent);
+console.log(population);
