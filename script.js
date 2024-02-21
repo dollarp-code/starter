4b29bb30cd8c1a4faef9edea5f92e919fb076519
dolapo <husseindollarp@yahoo.com>
2024-02-21 09:56:30 -0800

    Understanding Falsy values and Truthy values

--- script.js
+++ script.js
@@ -220,26 +220,49 @@ GOOD LUCK �  */
 
 // Type Conversion and coercion
 // conversion from Strings to Number
-const inputYear = "1991";
-console.log(Number(inputYear), inputYear);
-console.log(inputYear + 10);
-console.log(Number(inputYear) + 10);
+// const inputYear = "1991";
+// console.log(Number(inputYear), inputYear);
+// console.log(inputYear + 10);
+// console.log(Number(inputYear) + 10);
 
-console.log(Number("jonas"));
-console.log(typeof NaN);
+// console.log(Number("jonas"));
+// console.log(typeof NaN);
 
-// Conversion from Number to strings
-console.log(String(23), 23);
+// // Conversion from Number to strings
+// console.log(String(23), 23);
 
-// type coercion
-console.log("i am " + 23 + " years old");
-console.log("23" - "10" - 3);
-console.log("23" * "22");
-console.log("23" / "22");
+// // type coercion
+// console.log("i am " + 23 + " years old");
+// console.log("23" - "10" - 3);
+// console.log("23" * "22");
+// console.log("23" / "22");
 
-let n = "1" + 1;
-n = n - 1;
-console.log(n);
+// let n = "1" + 1;
+// n = n - 1;
+// console.log(n);
 
-console.log(2 + 3 + 4 + "5");
-console.log("20" - "5" - "4" - 2 + "5");
+// console.log(2 + 3 + 4 + "5");
+// console.log("20" - "5" - "4" - 2 + "5");
+
+// Falsy values and truthy values
+// 5 falsy values : 0, '', undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("jonas"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+const money = 0;
+if (money) {
+  console.log("Don't spend it all !!");
+} else {
+  console.log("You should get a job");
+}
+
+let height;
+if (height) {
+  console.log("YAY! Height is defined");
+} else {
+  console.log("Height is undefined");
+}
