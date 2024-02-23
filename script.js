86a0f57bca9315f09b8bc1332debb436671517f8
dolapo <husseindollarp@yahoo.com>
2024-02-22 20:48:10 -0800

    practicing switch statement

--- script.js
+++ script.js
@@ -480,3 +480,30 @@ console.log(
 // console.log(
 //   `The bill was ${bill}, the tip was ${tip}, and the total value ${totalValue}`
 // );
+
+const language = prompt("Enter your spoken language");
+
+switch (language) {
+  case "chinese":
+  case "mandarin":
+    console.log("Most number of native speakers");
+    break;
+
+  case "spanish":
+    console.log("2nd place in number of native speakers");
+    break;
+
+  case "english":
+    console.log("3rd place in number of native speakers");
+    break;
+
+  case "hindi":
+    console.log("4th place in number of native speakers");
+    break;
+
+  case "arabic":
+    console.log("5th most spoken language");
+    break;
+  default:
+    console.log("Great language too :D");
+}
