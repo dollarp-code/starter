97d5182ccac9d3228a5656b5aec7166299b886be
dolapo <husseindollarp@yahoo.com>
2024-02-22 19:10:23 -0800

    I attempted the challenge4 and i solved the problem

--- script.js
+++ script.js
@@ -425,20 +425,58 @@ GOOD LUCK */
 // console.log(`I'm ${2034 - 1998} years old ${me}`);
 
 // Conditional operator (Ternary operator)
-const age = 19;
-age >= 18
-  ? console.log("allowed to drink wine")
-  : console.log("allowed to drink water");
+// const age = 19;
+// age >= 18
+//   ? console.log("allowed to drink wine")
+//   : console.log("allowed to drink water");
 
-const drink = age >= 18 ? "wine" : "water";
-console.log(drink);
-console.log(`I like to drink ${age >= 18 ? "wine" : "water"}`);
-// using if else statement
+// const drink = age >= 18 ? "wine" : "water";
+// console.log(drink);
+// console.log(`I like to drink ${age >= 18 ? "wine" : "water"}`);
+// // using if else statement
 
-let drink2;
-if (age >= 18) {
-  drink2 = "wine";
-} else {
-  drink2 = "water";
-}
-console.log(drink2);
+// let drink2;
+// if (age >= 18) {
+//   drink2 = "wine";
+// } else {
+//   drink2 = "water";
+// }
+// console.log(drink2);
+
+// challenge 4
+/*Steven wants to build a very simple tip calculator for whenever he goes eating in a
+restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and
+300. If the value is different, the tip is 20%.
+Your tasks:
+1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for
+this. It's not allowed to use an if/else statement � (If it's easier for you, you can
+start with an if/else statement, and then try to convert it to a ternary
+operator!)
+2. Print a string to the console containing the bill value, the tip, and the final value
+(bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value
+316.25”
+Test data:
+§ Data 1: Test for bill values 275, 40 and 430
+Hints:
+§ To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+§ Value X is between 50 and 300, if it's >= 50 && <= 300  */
+
+//  using conditional operator
+const bill = 430;
+const tip = bill >= 50 && bill <= 300 ? bill * (15 / 100) : bill * (20 / 100);
+const totalValue = bill + tip;
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value ${totalValue}`
+);
+
+//using if else to solve
+// let tip;
+// if (bill >= 50 && bill <= 300) {
+//   tip = bill * (15 / 100);
+// } else {
+//   tip = bill * (20 / 100);
+// }
+// const totalValue = bill + tip;
+// console.log(
+//   `The bill was ${bill}, the tip was ${tip}, and the total value ${totalValue}`
+// );
